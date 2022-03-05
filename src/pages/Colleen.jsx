647cec21f01586d7ad02cb9c9b4eb702af28f93c
stitchineer <stitchineer@yahoo.com>
2022-03-04 22:35:55 -0500

    colleen initial commit

--- src/pages/Colleen.jsx
+++ src/pages/Colleen.jsx
@@ -7,7 +7,7 @@ export const Colleen = () => {
   return (
     <div className='page-container'>
       <div className='colleen-container'>
-      <p>hello</p>
+      <p>hello! i like pink</p>
 
       <Link to="/">
           <button className='btn-main'>back to main</button><br />
