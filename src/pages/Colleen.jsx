350edee148030a915648a97398f1ac0f1c41619d
stitchineer <stitchineer@yahoo.com>
2022-03-08 17:45:57 -0500

    music player

--- src/pages/Colleen.jsx
+++ src/pages/Colleen.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+// internal components and assets
+import MusicPlayer from '../components/MusicPlayer';
+import song from '../songs/in-the-hall-of-the-mountain-king.mp3';
+
+// scss
 import '../scss/Colleen.scss';
 
 export const Colleen = () => {
-  
+
   return (
     <div className='page-container'>
       <div className='colleen-container'>
-      <p>hello! i like pink</p>
 
-      <Link to="/">
+        <MusicPlayer song={song} />
+
+        <p>hello! i like pink</p>
+
+        <Link to="/">
           <button className='btn-main'>back to main</button><br />
-      </Link>
+        </Link>
       </div>
     </div>
   )
